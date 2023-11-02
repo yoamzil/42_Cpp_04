abfad7e9560d3c9386c707a420d381074f098529
Youness Amzil <amzilyouness@gmail.com>
2023-11-02 20:12:28 +0100

    First exercise is done.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -23,16 +23,19 @@
 
 int main()
 {
-	// const Animal *meta = new Animal();
-	// const Animal *j = new Dog();
-	// const Animal *i = new Cat();
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound(); // will output the cat sound!
-	// j->makeSound();
-	// meta->makeSound();
+	const Animal *meta = new Animal();
+	const Animal *j = new Dog();
+	const Animal *i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); // will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	delete meta;
+	delete i;
+	delete j;
 	//==================WrongAnimal example===================
-	const WrongAnimal	*test = new WrongCat();
-	std::cout << test->getType() << " " << std::endl;
+	// const WrongAnimal *i = new WrongCat();
+	// i->makeSound(); // will output the cat sound!
 	return (0);
 }
